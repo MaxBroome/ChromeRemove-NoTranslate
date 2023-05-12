d947ab8680e32f28b1d82b7c0def0a5f17cf358a
Max Broomfield <56701581+MaxBroome@users.noreply.github.com>
2023-05-11 23:33:01 -0500

    Delete Gimkit Allow Translate directory

--- Gimkit Allow Translate/contentScript.js
+++
@@ -1,9 +0,0 @@
-// contentScript.js
-function removeTags() {
-  const tags = document.querySelectorAll('[class="notranslate lang-en"]');
-  tags.forEach((tag) => {
-    tag.removeAttribute('class');
-  });
-}
-
-removeTags();
